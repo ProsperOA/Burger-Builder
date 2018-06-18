7d2eea6ef11d9ab1bf151df2ef3bbcacfa7265f0
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-06-17 21:58:52 -0700

    add base burger price, typing

--- src/models/ingredient.model.ts
+++ src/models/ingredient.model.ts
@@ -7,9 +7,11 @@ export type Ingredients = (
   | 'salad'
 );
 
-export const INGREDIENT_PRICES = {
+export const INGREDIENT_PRICES: {[name: string]: number} = {
   salad:  0.5,
   cheese: 0.4,
   meat:   1.3,
   bacon:  0.7
 }
+
+export const BASE_BURGER_PRICE: number = 1.50

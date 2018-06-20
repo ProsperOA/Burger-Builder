9975716deeea4cc96416e80ccfa8bde96cefb07a
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-06-20 11:42:23 -0700

    add catch all route

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route }  from 'react-router-dom';
+import { Route, Switch, Redirect }  from 'react-router-dom';
 
 import Auth          from './components/auth/auth.component';
 import Layout        from './components/layout/layout.component';
@@ -10,10 +10,13 @@ import Orders        from './components/orders/orders.component';
 const App: React.SFC<{}> = (props: {}): JSX.Element => (
   <div>
     <Layout>
-      <Route path="/" exact={true} component={BurgerBuilder} />
-      <Route path="/auth" component={Auth} />
-      <Route path="/checkout" component={Checkout} />
-      <Route path="/orders" component={Orders} />
+      <Switch>
+        <Route path="/" exact={true} component={BurgerBuilder} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Redirect to="/" />
+      </Switch>
     </Layout>
   </div>
 );
